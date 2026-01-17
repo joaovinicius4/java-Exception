f836a2b26c64b591d187cfe1eb6a9deeec225411
joaovinicius4 <joaoruas111@gmail.com>
2026-01-17 20:17:00 -0300

    fix: adiciona código da classe Reserva salvo corretamente

--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -1,5 +1,32 @@
 package model.entities;
 
-public class Reserva {
+import java.time.LocalDate;
 
+public class Reserva {
+	private Integer numero;
+	private LocalDate checkin;
+	private LocalDate checkout;
+	
+	public Reserva(Integer numero, LocalDate checkin, LocalDate checkout) {
+		this.numero = numero;
+		this.checkin = checkin;
+		this.checkout = checkout;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public LocalDate getCheckin() {
+		return checkin;
+	}
+
+	public LocalDate getCheckout() {
+		return checkout;
+	}	
+	
 }
