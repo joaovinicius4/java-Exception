1779b4299da02064581b5a8a6a03e2d476b28fb0
joaovinicius4 <joaoruas111@gmail.com>
2026-01-18 16:16:30 -0300

    feat: adiciona validacao de datas na atualizacao da reserva

--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -35,9 +35,19 @@ public class Reserva {
 	}
 	
 	public void atualizaReserva( LocalDate checkIn, LocalDate checkOut) {
+		
+		LocalDate now = LocalDate.now();
+		if(checkIn.isBefore(now) || checkOut.isBefore(now)) {
+			throw new IllegalArgumentException("As reservas precisam ser depois da data atual");
+		}
+		if(checkOut.isBefore(checkIn)) {
+			throw new IllegalArgumentException("O check tem que ser depois do check-in");
+		}
+		
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
 	}
+
 	
 	@Override
 	public String toString() {
