3991d591181e5705675753b0751a073e92f87482
joaovinicius4 <joaoruas111@gmail.com>
2026-01-18 16:05:48 -0300

    feat: adiciona tratamento basico de excecoes na criacao e atualizacao de reserva

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import model.entities.Reserva;
@@ -13,20 +15,35 @@ public class Programa {
 		Scanner sc = new Scanner(System.in);
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		
-		System.out.print("Numero do quarto: ");
-		int numero = sc.nextInt();
-		System.out.print("Data de check-in (dd/MM/yyyy): ");
-	    LocalDate checkIn = LocalDate.parse(sc.next(), dtf);
-	    System.out.print("Data de check-out (dd/MM/yyyy): ");
-	    LocalDate checkOut = LocalDate.parse(sc.next(), dtf);
-	    
-	    Reserva reserva = new Reserva(numero, checkIn, checkOut);
-
-        System.out.println();
-        System.out.println("Reserva: " + reserva);
-        
+		try {
+			System.out.print("Numero do quarto: ");
+			int numero = sc.nextInt();
+			System.out.print("Data de check-in (dd/MM/yyyy): ");
+		    LocalDate checkIn = LocalDate.parse(sc.next(), dtf);
+		    System.out.print("Data de check-out (dd/MM/yyyy): ");
+		    LocalDate checkOut = LocalDate.parse(sc.next(), dtf);
+		    
+		    Reserva reserva = new Reserva(numero, checkIn, checkOut);
+		    System.out.println("Reserva: "+ reserva);
+		    
+		    
+		    System.out.println("Entre com a nova data de reserva: ");
+		    System.out.println("Check-in: ");
+		    checkIn = LocalDate.parse(sc.next(), dtf);
+		    System.out.print("Check-out: ");
+		    checkOut = LocalDate.parse(sc.next(), dtf);
+		    reserva.atualizaReserva(checkIn, checkOut);
+		    System.out.println("Reserva: "+ reserva);
+		}
+        catch(InputMismatchException e) {
+        	System.out.println("Deve ser digitado numero inteiro para o numero do quarto! ");
+        }
+		catch(DateTimeParseException e) {
+			System.out.println("Formato de data digitado errado!");
+		}
         
 		sc.close();
 	}
+
 
 }
