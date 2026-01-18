5a51a27105f7f4ff27cc9d8b82912d70bcf2fcd8
joaovinicius4 <joaoruas111@gmail.com>
2026-01-18 11:28:54 -0300

    feat: adiciona cálculo de duração e atualização de datas da reserva

--- src/model/entities/Reserva.java
+++ src/model/entities/Reserva.java
@@ -23,6 +23,14 @@ public class Reserva {
 
 	public LocalDate getCheckOut() {
 		return checkOut;
-	}	
+	}
 	
+	public long duracao() {
+	    return java.time.temporal.ChronoUnit.DAYS.between(checkIn, checkOut); // uso de API de tempo que nao conhecia
+	}
+	
+	public void atualizaReserva( LocalDate checkIn, LocalDate checkOut) {
+		this.checkIn = checkIn;
+		this.checkOut = checkOut;
+	}
 }
